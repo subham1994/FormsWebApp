926184a29545a68cc8eb78a628dccd51029e3125
Subham gaurav <shubham.gaurav.1994@gmail.com>
2016-02-24 18:26:05 +0530

    added preview page

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var express = require('express'),
-     app = express(),
-     PORT = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static('app'));
 
