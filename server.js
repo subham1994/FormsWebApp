ae5bc1e5485bb4ea7db78160faf5d287bfcd7d63
Subham gaurav <shubham.gaurav.1994@gmail.com>
2016-02-25 19:06:57 +0530

    fixed date format

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ var PORT = process.env.PORT || 3000;
 app.use(express.static('app'));
 
 app.listen(PORT, function() {
-    console.log('Express listening on port 8080');
+    console.log('Express listening on port 3000');
 });
