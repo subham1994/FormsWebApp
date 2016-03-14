495504f240bae677a39c4ec897222fed60941a02
KodeWiki <nishant.ece.iitkgp@gmail.com>
2016-03-14 02:14:25 -0700

    added a comment

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.static('app'));
 
-app.listen(PORT, function() {
+app.listen(PORT, function() { // start the server
     console.log('Express listening on port 3000');
 });
